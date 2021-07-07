f4bb51021f9242ea876393f5a1551e325c057e53
Balaji735 <balajiweb99@gmail.com>
2021-07-07 08:37:20 +0530

    WH-8520 - First Time File Update

---
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -0,0 +1,12 @@
+package com.example.demo.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("WELCOME TO SPRING BOOT APP");
+	}
+
+}
