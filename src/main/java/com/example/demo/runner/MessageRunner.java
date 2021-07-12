be43f53c6105d3123db48ffec9b8c3b217c0e3dd
Balaji735 <balajiweb99@gmail.com>
2021-07-12 19:05:59 +0530

    WH-4125 UPDATE RUNNER WITH LOG4J SLF4J

--- src/main/java/com/example/demo/runner/MessageRunner.java
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -1,14 +1,18 @@
 package com.example.demo.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 
 public class MessageRunner implements CommandLineRunner {
-
+  private static Logger log=LoggerFactory.getLogger(MessageRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
 		int a=10;
+		log.info("STARTED");
 		System.out.println("WELCOME TO SPRING BOOT APP!"+a);
 		System.out.println("File Modified");
+		log.info("END");
 	}
 
 }
